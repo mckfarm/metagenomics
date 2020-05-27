eb98181c6fe259deefdf48f10c0eaa8ea90bf22f
mckenna19 <51832180+mckenna19@users.noreply.github.com>
2020-05-27 18:52:09 -0500

    fastqc, bowtie mapping
    added fastqc automatic output script and bowtie mapping

---
+++ bash/1.1_fastqc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name="fastqc"
+#SBATCH -A b1042
+#SBATCH -p genomicsguest
+#SBATCH -t 10:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=10
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=mckennafarmer2023@u.northwestern.edu
+#SBATCH --output=outlog_fastqc
+#SBATCH --error=errorlog_fastqc
+
+module purge all
+module load fastqc
+
+cd /projects/b1052/Wells_b1042/McKenna/training/SG_Metagenome/clean_reads
+
+fastqc SG_01_11_2017_S17_R1_trimmed.fq SG_01_11_2017_S17_R2_trimmed.fq SG_05_27_2016_S16_R1_trimmed.fq SG_05_27_2016_S16_R2_trimmed.fq
---
+++ bash/5.1_DNA_bowtie_mapping2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --job-name="bowtie2"
+#SBATCH -A b1042
+#SBATCH -p genomicsguest
+#SBATCH -t 20:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=10
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=mckennafarmer2023@u.northwestern.edu
+#SBATCH --output=outlog_bowtie2
+#SBATCH --error=errorlog_bowtie2
+
+BASENAME="SG_01_11_2017"
+
+module purge all
+module load bowtie2/2.2.6
+module load samtools/1.2
+module load perl/5.16
+
+cd /projects/b1052/Wells_b1042/McKenna/training/SG_Metagenome/
+
+#building mapping files from your contigs assembled
+bowtie2-build ./IDBA/assembly_SG_01_11_2017/scaffold_01_11_2017.fa ./mapped_reads/$BASENAME
+
+#bowtie mapping
+bowtie2 --threads 10 -q -x ./mapped_reads/$BASENAME -1 ./clean_reads/$BASENAME.1.trimmed.fq -2 ./clean_reads/$BASENAME.2.trimmed.fq -S $BASENAME.bowtie2.sam
+samtools view -bS $BASENAME.bowtie2.sam > $BASENAME.bowtie2.bam
+samtools sort $BASENAME.bowtie2.bam $BASENAME.bowtie2.sorted
+samtools depth $BASENAME.bowtie2.sorted.bam > $BASENAME.depth.txt
+#end mapping
+
+#to generate the coverage and length info of each contig based on the mapping results
+#the coverage info generated could be used to calculate the abundance of each genome bin obtained; and the ratio of reads used to generate the contigs assembled; and the DNA/RBA RPKM values of each gene or genome
+perl ./scripts/calc.coverage.in.bam.depth.pl -i $BASENAME.depth.txt -o $BASENAME.coverage.length.csv
