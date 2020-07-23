d3d25989cd42a073786e166ae6c357f2135cfc81
mckfarm <51832180+mckfarm@users.noreply.github.com>
2020-07-23 14:10:14 -0500

    updated classify script
    troubleshooting for gtdb-tk memory issue, using a scratch dir (which temporarily writes ~110 GB on the group allocation) allows the program to run completely

--- bash/9_GTDB_classify.sh
+++ bash/9_GTDB_classify.sh
@@ -2,16 +2,15 @@
 #SBATCH --job-name="gtdb_classify"
 #SBATCH -A b1042
 #SBATCH -p genomicsguest
-#SBATCH -t 07:00:00
+#SBATCH -t 20:00:00
 #SBATCH -N 1
-#SBATCH --mem=100G
-#SBATCH --ntasks-per-node=1
+#SBATCH --mem=110G
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=mckennafarmer2023@u.northwestern.edu
 #SBATCH --output=outlog_gtdb_cla
 #SBATCH --error=errorlog_gtdb_cla
 
-
+# mem-per-cpu=11G
 module load python/anaconda3.6
 
 # export PATH=”$PATH：/projects/b1052/pythonenvs/python3.6/gtdb/site-packages/gtdbtk/“
@@ -19,8 +18,10 @@ module load python/anaconda3.6
 # pip install matplotlib
 # pip install numpy
 
-source activate /projects/b1052/pythonenvs/python3.6/gtdb
+source activate /projects/b1052/pythonenvs/python3.6/gtdbtk
 
 cd /projects/b1052/Wells_b1042/McKenna/training/SG_Metagenome
 
-gtdbtk classify --debug --align_dir ./gtdbtk_bins --cpus 1 --extension fa --genome_dir ./bins_final --out_dir ./gtdbtk_bins
+# gtdbtk classify --debug --align_dir ./gtdbtk_bins --pplacer_cpus 10 --extension fa --genome_dir ./bins_final --out_dir ./gtdbtk_bins
+
+gtdbtk classify --debug --align_dir ./gtdbtk_bins --extension fa --genome_dir ./bins_final --out_dir ./gtdbtk_bins --scratch_dir ./scratch
